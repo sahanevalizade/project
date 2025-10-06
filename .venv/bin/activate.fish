91a5d9999bca36074328cf03e07f085d27a1954c
sahanevalizade <shanvlizad0@gmail.com>
2025-10-06 17:26:19 +0400

    qswgd

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/shahane/project/.venv"
+set -gx VIRTUAL_ENV "/home/shahane/project-1/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
